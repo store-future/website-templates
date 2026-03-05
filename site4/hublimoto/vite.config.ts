4bac8970be5577566b61da9dc24f475298625ea6
store-future <dhananjayasharma43@gmail.com>
2026-03-06 02:19:23 +0530

    fix deployment of site 4

--- site4/hublimoto/vite.config.ts
+++ site4/hublimoto/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website-templates/site4/hublimoto/dist/'
+  base: './'
 
 })
